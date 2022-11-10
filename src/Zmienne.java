bf11d12a0aa014b81f579643511003223f056e7e
przemjan <przemjanisz2@gmail.com>
2022-11-10 08:24:52 +0100

    Initial commit

---
+++ src/Zmienne.java
@@ -0,0 +1,38 @@
+public class Zmienne {
+    public static void main(String[] args) {
+
+        int liczba1;
+        liczba1 = 123;
+
+        double liczba2 = 23.9;
+
+
+        System.out.println("Moja zmienna: " + liczba1);
+
+        liczba1 = (int)liczba2;
+
+        System.out.println(liczba1);
+
+
+        int workHoursPerYear;
+        int hours = 8;
+        int days = 5;
+        int weeks = 52;
+        //komentarz
+
+        workHoursPerYear = hours * days * weeks;
+
+        String napis = "Dzisiaj\n jest \"ładna\" pogoda";
+
+        System.out.println("Mó\tj napis: " + napis + ".");
+        System.out.print("Mój napis: " + napis + ".\n");
+        System.out.print("Mój napis: " + napis + ".\n");
+
+        char znak = '5';
+
+        boolean isSlonce = false; //true
+
+        System.out.println("Mój wynik " +  liczba2);
+
+    }
+}
