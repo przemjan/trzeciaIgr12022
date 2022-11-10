db5d83139ba9dfc4214d6dcf057b6c20dfac77c3
przemjan <przemjanisz2@gmail.com>
2022-11-10 09:54:06 +0100

    commit nr 2

---
+++ src/KOmpNaOsoba.java
@@ -0,0 +1,14 @@
+public class KOmpNaOsoba {
+    public static void main(String[] args) {
+
+        int osoba = 9;
+
+        int komp = 13;
+
+        double kompNaOsoba;
+        kompNaOsoba = (double)komp / osoba;
+
+        System.out.println("Ilośc kompów na osobę: " + kompNaOsoba);
+
+    }
+}
